9a4733e96b285033e4428cc1628d14b56910ee5d
stephanie2020 <55030152+stephanie2020@users.noreply.github.com>
2019-10-15 10:10:44 -0400

    Update errors in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
-console.log("Welcome to my portfolio! Have a look around.")
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+console.log('Welcome to my portfolio! Have a look around.')
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
-
 }
